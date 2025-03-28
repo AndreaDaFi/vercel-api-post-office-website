36da61c986efe2f68237cd81279e8b8f815bd3b1
Jennifer Nava <jennifernava681@gmail.com>
2025-03-27 20:49:19 -0500

    fix

--- api/getCustomerMessages.js
+++ api/getCustomerMessages.js
@@ -1,3 +1,4 @@
+import mysql from 'mysql2/promise';
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
